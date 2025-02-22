c9dcb63fa25a9fdf89e12f0f972baca7c4321f54
dcalmeida <danielalmeida.dca@outlook.com>
2025-02-22 06:24:58 -0300

    teste commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,8 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Aluno: Daniel Almeida
+// Matricula: 202503008515
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
